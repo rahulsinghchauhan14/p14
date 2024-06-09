aa77dd8bde785dce2a71e97ea51f63e1250f495d
Rahul Singh Chauhan <rahulsinghchauhan14@gmail.com>
2024-06-09 19:03:42 +0530

    complete first part of the assignment intro to react

---
+++ intro-to-react/src/BookList.js
@@ -0,0 +1,23 @@
+
+const books = [
+    { title: 'Book 1', author: 'Author 1', year: 2020 },
+  { title: 'Book 2', author: 'Author 2', year: 2018 },
+  { title: 'Book 3', author: 'Author 3', year: 2022 },
+  { title: 'Book 4', author: 'Author 3', year: 2020 },
+  { title: 'Book 5', author: 'Author 1', year: 2018 },
+  { title: 'Book 6', author: 'Author 4', year: 2022 },
+]
+
+function BookList(){
+    return(
+       <ul>
+            { books.map((book, index) => (
+                <li key={index}>
+                    <strong>{book.title}</strong> by {book.author} ({book.year})
+                </li>
+            ))}
+       </ul>
+    );
+}
+
+export default BookList;
